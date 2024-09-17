adb98226ed8fa0175d5bbd796d57adca4ed49629
Mihaeldzh <pplesho@gmail.com>
2024-09-17 23:30:31 +0300

    Tailwind config

---
+++ ChatBot-BOTv/BOTv/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
