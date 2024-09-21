3b6b5b55ce4f7ef1473011158c5d0c7309d9dda5
Mihaeldzh <pplesho@gmail.com>
2024-09-21 19:22:12 +0300

    new api methods

--- ChatBot-BOTv/BOTv/src/Api.js
+++ ChatBot-BOTv/BOTv/src/Api.js
@@ -1,3 +1,26 @@
+export const getData = async (url = '') => {
+  try {
+      const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+      });
+
+      if (!response.ok) {
+          throw new Error('Response was not ok');
+      }
+
+      const result = await response.json();
+      console.log(result);
+
+      return result;
+  } catch (error) {
+      console.error('Error fetching data:', error);
+  }
+};
+
+
 export const postData = async (url = '', data = {data:"ex"}) => {
     
       const response = await fetch(url, {
@@ -17,4 +40,23 @@ export const postData = async (url = '', data = {data:"ex"}) => {
 
       return result;
      
+  };
+
+  export const updateData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+  
+    if (!response.ok) {
+      throw new Error('Response was not ok');
+    }
+  
+    const result = await response.json();
+    console.log("updated");
+  
+    return result;
   };
